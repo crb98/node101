40a5ab94793e8024ee5082dabd47892a04efb769
crb98 <conniebusby98@gmail.com>
2026-02-18 11:19:51 +0000

    all tests pass

--- worksheet/utility.js
+++ worksheet/utility.js
@@ -11,6 +11,29 @@ export function subtract(a, b) {
  * "export" to the function definition (as seen in the
  * subtract example above)
  */
-function add(a, b) {
+export function add(a, b) {
   return a + b;
+}
+
+export function compare(a, b) {
+  return a.length === b.length && a.every(function(v, i) {
+    return v === b[i];
+  });
+}
+
+export function largest(arr) {
+  if (arr.length === 0) return null;
+  return Math.max(...arr);
+}
+
+export function zeroest(arr) {
+  return arr.reduce((closest, current) => {
+    const absClosest = Math.abs(closest);
+    const absCurrent = Math.abs(current);
+    
+    if (absCurrent < absClosest || (absCurrent === absClosest && current > closest)) {
+      return current;
+    }
+    return closest;
+  });
 }
